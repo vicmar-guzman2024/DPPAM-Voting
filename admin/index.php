6259f88da8caf8c6ed98bc8976c92aa2e1348276
chris03312 <catalan.christian.03312002@gmail.com>
2024-12-11 01:42:42 +0800

    another one

--- admin/index.php
+++ admin/index.php
@@ -28,8 +28,6 @@ include ("php/dashboard.php");
    <!--JS CHART CDN-->
    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-g
   
   
 
